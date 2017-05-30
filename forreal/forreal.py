35173deae3ba2c45af3f12cdd1f106f3605129d3
SarekSJ <syke117@yahoo.com>
2017-05-30 13:26:34 -0700

    Added more stuff to forreal.py

--- forreal/forreal.py
+++ forreal/forreal.py
@@ -1,4 +1,5 @@
 import pprint
+import math
 def gather_data_from_training():
     users = []
     with open('train.txt') as f:
@@ -30,23 +31,51 @@ def collect_user_preliminary_data():
         sucking_dicts_aint_gay: dictionary containing user info and ratings to base predictions off of
     """
     sucking_dicts_aint_gay = {}
-    
+
 
     with open ('test5.txt') as f:
         for line in f:
             split_line = line.split(' ')
             try:
                 if split_line[2][0:1] != '0':
-                    sucking_dicts_aint_gay[split_line[0]][split_line[1]] = int(split_line[2][0:1])
+                    sucking_dicts_aint_gay[int(split_line[0])][int(split_line[1])] = int(split_line[2][0:1])
             except Exception as e:
-                sucking_dicts_aint_gay[split_line[0]] = {}
+                sucking_dicts_aint_gay[int(split_line[0])] = {}
                 if split_line[2][0:1] != '0':
-                    sucking_dicts_aint_gay[split_line[0]][split_line[1]] = int(split_line[2][0:1])
+                    sucking_dicts_aint_gay[int(split_line[0])][int(split_line[1])] = int(split_line[2][0:1])
     return sucking_dicts_aint_gay
 
 users = collect_user_preliminary_data()
 train_users = gather_data_from_training()
 # print (users)
+
+def generate_cosine_similarity(users, training_data, user_index, train_index):
+    # users[user_index] = dictionary of all the ratings they have
+    # ex: users[user_index] = {'237': 4, '306': 5, ... , '934': 5}
+    train_user = []
+    test_user  = []
+    numerator = 0
+
+    for movie, rating in users[user_index].items():
+        train_rating = training_data[int(train_index)-1][int(movie)-1]
+        if train_rating == 0:
+            continue
+        else:
+            train_user.append(train_rating)
+            test_user.append(rating)
+            numerator += train_rating * rating
+    train_den = 0
+    test_den = 0
+    for i in range(len(test_user)):
+        train_den += train_user[i] ** 2
+        test_den += test_user[i] ** 2
+    if ((math.sqrt(train_den)) * (math.sqrt(test_den))) == 0:
+        return 0
+    else:
+        if len(train_user) == 1:
+            return 1 / (abs(train_den - test_den) + 5)
+        else:
+            return numerator / ((math.sqrt(train_den)) * (math.sqrt(test_den)))
 
 def find_similar_users(test_users, training_users):
     similar_users = {}
@@ -65,20 +94,14 @@ def find_similar_users(test_users, training_users):
         similar_users[user_id] = {}
         for movie, rating in movies_dict.items():
             for index, train_user in enumerate(training_users):
-                if train_user[int(movie)] != 0:
+                if train_user[movie] != 0:
                     # Then we can consider their similarity, add them to the list of users who rated the same movies
-                    similar_users[user_id][index] = train_user[int(movie)]
-            # for train_user in similar_users:
-            #     similar_users[train_user] = generate_cosine_similarity(test_users, training_users, user_id, train_user)
-    print(similar_users)
+                    similar_users[user_id][index] = .4
+            for train_user in similar_users[user_id]:
+                print(user_id, train_user)
+                similar_users[user_id][train_user] = generate_cosine_similarity(test_users, training_users, user_id, train_user)
     return similar_users
 
 similar_users = find_similar_users(users, train_users)
+pprint.pprint(similar_users)
 
-def generate_cosine_similarity(users, training_data, user_index, train_index):
-    # users[user_index] = dictionary of all the ratings they have
-    # ex: users[user_index] = {'237': 4, '306': 5, ... , '934': 5}
-    for movie, rating in users[user_index]:
-        train_rating = training_data[train_index][movie]
-
-    pass
